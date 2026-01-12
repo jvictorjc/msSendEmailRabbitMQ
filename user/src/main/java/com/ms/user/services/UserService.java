f45f4cb89321e698d955d6925be08e4b471b1422
joao.carvalho <joao.carvalho@goias.gov.br>
2026-01-12 12:16:16 -0300

    Initial commit - adicionando microsserviços email e user

---
+++ user/src/main/java/com/ms/user/services/UserService.java
@@ -0,0 +1,24 @@
+package com.ms.user.services;
+
+import com.ms.user.models.UserModel;
+import com.ms.user.producers.UserProducer;
+import com.ms.user.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository  userRepository;
+    private final UserProducer userProducer;
+
+    @Transactional
+    public UserModel save(UserModel userModel){
+        userModel = userRepository.save(userModel);
+        userProducer.publishMessageEmail(userModel);
+        return userModel;
+    }
+
+}
