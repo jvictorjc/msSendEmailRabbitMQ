a8be73580feea83a09cb1bdcf4c3aaa1aa2c2519
joao.carvalho <joao.carvalho@goias.gov.br>
2026-01-13 19:30:22 -0300

    disable test for User

--- user/src/test/java/com/ms/user/UserApplicationTests.java
+++ user/src/test/java/com/ms/user/UserApplicationTests.java
@@ -1,8 +1,10 @@
 package com.ms.user;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled("Desabilitado no CI: teste de contexto requer infraestrutura (Rabbit/DB)")
 @SpringBootTest
 class UserApplicationTests {
 
